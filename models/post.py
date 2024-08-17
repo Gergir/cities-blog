6cd64378f041edf6be4202e022f827844083e8b9
Dawid Gruszka <aloneviahome@gmail.com>
2024-08-17 12:43:10 +0200

    database service added, endpoints CRUD created

--- models/post.py
+++ models/post.py
@@ -1,9 +1,28 @@
 from pydantic import BaseModel
 from datetime import datetime
+from services.db_service import Base, engine
+from sqlalchemy import Column, Integer, String, DateTime
+
+class Post(Base):
+    __tablename__ = "posts"
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    subtitle = Column(String)
+    date = Column(DateTime)
+    content = Column(String)
+
+Base.metadata.create_all(bind=engine)
 
 
-class Post(BaseModel):
+class PostRequest(BaseModel):
     title: str
     subtitle: str | None = None
     date: datetime = datetime.now()
+    content: str
+
+
+class PostResponse(BaseModel):
+    id: int
+    title: str
+    date: datetime
     content: str
--- routers/post.py
+++ routers/post.py
@@ -1,35 +1,56 @@
 from typing import Annotated
-
-from fastapi import APIRouter, Body
-from models.post import Post
+from fastapi import APIRouter, Body, Depends, HTTPException
+from sqlalchemy.orm import Session
+from models.post import Post, PostRequest, PostResponse
+from services.db_service import get_db
 
 
 router = APIRouter(prefix="/post", tags=["post"])
 
 
 @router.get("/all")
-def get_all_posts():
-    return {"message": "fetching all posts"}
+def get_all_posts(db: Session = Depends(get_db)):
+    db_post = db.query(Post).all()
+    return db_post
 
 
-@router.get("/{post_id}")
-def get_post(post_id: int):
-    return {"post_id": post_id}
+@router.get("/{post_id}", response_model=PostResponse)
+def get_post(post_id: int, db: Session = Depends(get_db)):
+    db_post = db.query(Post).filter(Post.id == post_id).first()
+    if not db_post:
+        raise HTTPException(status_code=404, detail=f"Entity with id {post_id} not found")
+    return db_post
 
 
-@router.post("/new")
-def create_post(post: Annotated[Post, Body()]):
-    return post
+@router.post("/new", response_model=PostResponse)
+def create_post(post: Annotated[PostRequest, Body()], db: Session = Depends(get_db)):
+    db_post = Post(**post.model_dump())
+    db.add(db_post)
+    db.commit()
+    db.refresh(db_post)
+    return db_post
 
 
-@router.patch("/update/{post_id}")
-def update_post(post_id: int, post: Annotated[Post, Body()]):
-    return {
-        "post_id": post_id,
-        "post": post
-    }
+@router.patch("/update/{post_id}", response_model=PostResponse)
+def update_post(post_id: int, post: Annotated[PostRequest, Body()], db: Session = Depends(get_db)):
+    db_post = db.query(Post).filter(Post.id == post_id).first()
+    if not db_post:
+        raise HTTPException(status_code=404, detail=f"Entity with id {post_id} not found")
+
+    for key, value in post.dict(exclude_unset=True).items():
+        setattr(db_post, key, value)
+
+    db.commit()
+    db.refresh(db_post)
+
+    return db_post
 
 
 @router.delete("/delete/{post_id}")
-def delete_post(post_id):
+def delete_post(post_id: int, db: Session = Depends(get_db)):
+    db_post = db.query(Post).filter(Post.id == post_id).first()
+    if not db_post:
+        raise HTTPException(status_code=404, detail=f"Entity with id {post_id} not found")
+    db.delete(db_post)
+    db.commit()
     return {"message": f"post with id {post_id} deleted"}
